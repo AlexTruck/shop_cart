d9ebf91c407c3ccaf6fa3b5ef451957470ef9598
AlexTruck <alexandr.kujba@gmail.com>
2015-08-07 11:06:47 +0300

    create habtm association for models

--- app/models/admin/discount.rb
+++ app/models/admin/discount.rb
@@ -1,4 +1,6 @@
 class Admin::Discount < ActiveRecord::Base
+  has_and_belongs_to_many :products
+
   validates_presence_of :title, :description, :amount, :started_at, :ended_at
   validates :variant, presence: true, inclusion: { in: %w(% uah), message: 'It is not a valid size' }
 end
